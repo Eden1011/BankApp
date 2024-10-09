c959af8133456c113b73130ba2536419a1dfaaab
Eden1011 <glovkakrzys252@gmail.com>
2024-10-09 16:56:31 +0200

    sprawdzenie pesela

--- app/Konto.py
+++ app/Konto.py
@@ -3,4 +3,6 @@ class Konto:
         self.imie = imie
         self.nazwisko = nazwisko
         self.pesel = pesel
+        if len(f"{self.pesel}") != 11:
+            self.pesel = "Pesel nie jest poprawny!"
         self.saldo = 0
